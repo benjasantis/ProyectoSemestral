b3979c1f366f86bd600e5645f9711c1085f63ba2
benjasantis <be.santis@duocuc.cl>
2021-05-05 13:50:44 -0400

    nuevo formulario colaborador

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,47 @@
 function mensaje() {
-    alert('Su mensaje ha sido enviado, nos contactaremos a la brevedad');
+    alert('Su mensaje ha sido enviado, nos contactaremos a la brevedad')
+    window.open('index.html')
+    window.close();
 }
 
 function borrar(){
     alert('Elementos eliminados');
+}
+
+function ingresar(){
+    alert('Ingreso Correcto')
+    window.open('index.html')
+    window.close();
+
+
+}
+
+function registrar(){
+    window.open('registro.html')
+    window.close();
+}
+
+function registro(){
+    alert('Registro exitoso')
+    window.open('index.html')
+    window.close();
+}
+
+function ingrColaborador(){
+    alert('Ingreso Correcto')
+    window.open('registro_noticia.html')
+    window.close();
+
+}
+
+function ayuda(){
+    window.open('ayuda.html')
+    window.close();
+
+}
+
+function enviarnot() {
+    alert('Su noticia a sido enviada, el adm. revisara y publicara')
+    window.open('index.html')
+    window.close();
 }
